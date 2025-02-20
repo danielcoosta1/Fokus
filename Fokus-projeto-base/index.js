8bc0f8b021929f12f15b4377dc6c3a709bfc9793
Daniel Costa <daniel.dc697@gmail.com>
2025-02-20 14:01:08 -0300

    colocando a dinamica da alteração do texto e do botão

--- Fokus-projeto-base/index.js
+++ Fokus-projeto-base/index.js
@@ -7,24 +7,65 @@ const longoButton = document.querySelector(".app__card-button--longo");
 
 const imgBanner = document.querySelector(".app__image");
 
+const appTitulo = document.querySelector(".app__title");
+
+
 focoButton.addEventListener("click", () => {
     alterarContexto("foco");
-    
+
     
 });
 
 curtoButton.addEventListener("click", () => {
     alterarContexto("descanso-curto");
-    
+   
 });
 
 longoButton.addEventListener("click", () => {
     alterarContexto("descanso-longo");
+  
 });
 
-
-
 function alterarContexto(contexto){
+
+    // ALTERANDO O DATA-ATRIBUTE DO HTML
     html.setAttribute("data-contexto", contexto);
+
+    // ALTERANDO A IMAGEM DE FUNDO
     imgBanner.setAttribute("src", `./imagens/${contexto}.png`);
+    
+    
+    // ALTERANDO O TITULO PRINCIPAL DA PAGINA e O BOTÃO ATIVADO
+    switch (contexto) {
+        case "foco":
+            appTitulo.innerHTML =  `Otimize sua produtividade,<br>
+            
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            focoButton.classList.add("active");
+            longoButton.classList.remove("active");
+            curtoButton.classList.remove("active");
+            break;
+        
+        case "descanso-curto":
+             appTitulo.innerHTML =  `Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            focoButton.classList.remove("active");
+            longoButton.classList.remove("active");
+            curtoButton.classList.add("active");
+            break;
+
+        default:
+            appTitulo.innerHTML =  `Hora de voltar à superfície.<br>
+            
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+            focoButton.classList.remove("active");
+            longoButton.classList.add("active");
+            curtoButton.classList.remove("active");
+            
+            break;
+    }
+ 
+    // ALTERANDO O BOTÃO PARA ATIVADO
+
+
 }
