4706c17014874dd7bdbfe3ca499ea7f7294fd903
Daniel Costa <daniel.dc697@gmail.com>
2025-02-21 09:31:43 -0300

    alterando o botão começar/pausar

--- Fokus-projeto-base/index.js
+++ Fokus-projeto-base/index.js
@@ -1,3 +1,4 @@
+// VARIÁVEIS
 const html = document.querySelector("html");
 
 //BOTÕES
@@ -19,7 +20,6 @@ const musicaInput = document.querySelector("#alternar-musica");
 const musica = new Audio("./sons/luna-rise-part-one.mp3");
 musica.loop = true;
 
-
 musicaInput.addEventListener("change", ()=>{
     if(musica.paused){
         musica.play();
@@ -28,23 +28,40 @@ musicaInput.addEventListener("change", ()=>{
     }
 });
 
+//TEMPORIZADOR
 
+let tempoDecorridoSegundos = 60;
+
+
+//BOTÃO DO TIMER :  COMEÇAR/PAUSAR
+const startPauseBt = document.querySelector("#start-pause");
+const imagemStartPauseBt = startPauseBt.querySelector("img");
+const spanStartPauseBt = startPauseBt.querySelector("span");
+
+//ELEMENTO QUE MOSTRA O TEMPO DECORRIDO
+const tempo = document.querySelector("#timer");
+
+
+///////////////////////////////////
+
+// FUNÇOES E EVENTOS
+
+//EVENTO DOS BOTOES- FOCO/DL/DC/
 
 focoButton.addEventListener("click", () => {
     alterarContexto("foco");
 
-    
 });
 
 curtoButton.addEventListener("click", () => {
     alterarContexto("descanso-curto");
-   
 });
 
 longoButton.addEventListener("click", () => {
     alterarContexto("descanso-longo");
-  
 });
+
+// FUNÇÃO ALTERAR CONTEXTO -  ESTILO DO BANNER E ESTILO DO BOTÃO SELECIONADO
 
 function alterarContexto(contexto){
 
@@ -60,7 +77,6 @@ function alterarContexto(contexto){
         case "foco":
             // ALTERANDO TEXTO
             appTitulo.innerHTML =  `Otimize sua produtividade,<br>
-            
             <strong class="app__title-strong">mergulhe no que importa.</strong>`
            
            //ALTERANDO O ESTILO DO BOTÃO ATIVADO
@@ -68,38 +84,73 @@ function alterarContexto(contexto){
             longoButton.classList.remove("active");
             curtoButton.classList.remove("active");
 
-           
+            //ADICIONANDO O TIMER NA TELA
+            
 
+            
+            
             break;
         
         case "descanso-curto":
-
+             // ALTERANDO TEXTO
              appTitulo.innerHTML =  `Que tal dar uma respirada?<br>
             <strong class="app__title-strong">Faça uma pausa curta!</strong>`
-            
+             //ALTERANDO O ESTILO DO BOTÃO ATIVADO
             focoButton.classList.remove("active");
             longoButton.classList.remove("active");
             curtoButton.classList.add("active");
-            
-            
-
+            //ADICIONANDO O TIMER NA TELA
+           
             break;
 
         default:
+             // ALTERANDO TEXTO
             appTitulo.innerHTML =  `Hora de voltar à superfície.<br>
-            
             <strong class="app__title-strong">Faça uma pausa longa.</strong>`
             
+            //ALTERANDO O ESTILO DO BOTÃO ATIVADO
             focoButton.classList.remove("active");
             longoButton.classList.add("active");
             curtoButton.classList.remove("active");
-
+            //ADICIONANDO O TIMER NA TELA
             
+
             
             break;
     }
  
-    
+}
 
+//TIMER
+
+startPauseBt.addEventListener("click", ()=>{
+
+    
+    if (spanStartPauseBt.textContent === 'Começar') {
+        // Altera o texto para "Pausar"
+        spanStartPauseBt.textContent = 'Pausar';
+        // Altera o src da imagem para o ícone de pausa
+        imagemStartPauseBt.src = './imagens/pause.png';
+
+
+    } else {
+        // Altera o texto de volta para "Começar"
+       spanStartPauseBt.textContent = 'Começar';
+        // Altera o src da imagem de volta para o ícone de play
+       imagemStartPauseBt.src = './imagens/play_arrow.png';
+
+    }
+
+});
+
+const contagemRegressiva = () => {
+    setInterval(() => {
+        tempoDecorridoSegundos -= 1;
+    }, 1000);
+
+    setInterval(() => {
+        tempoDecorridoMinutos -=1;
+    },60000);
 
 }
+
